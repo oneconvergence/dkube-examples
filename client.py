672015c9da5c5f1569a5a2f7a2a34d3028fd8605
mak-454 <ahmed.khan@oneconvergence.com>
2019-10-14 07:42:09 -0700

    Added the training launcher component code.

--- client.py
+++ client.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     token = 'eyJTZXNzaW9uIjp0cnVlLCJUb2tlbiI6ImV5SmhiR2NpT2lKSVV6STFOaUlzSW5SNWNDSTZJa3BYVkNKOS5leUpqY21WaGRHVmtJam94TlRjd01qVTNNakE1TENKeWIyeGxJam9pYjNCbGNtRjBiM0lpTENKMWMyVnlibUZ0WlNJNkltOWpaR3QxWW1VaWZRLlBsYjZMNEhDME16ZDlPLTItS1hrQVdyVGRyV2xCaGM1bGEyVEhCWk1kejQifQ=='
     env = Environment(ip='192.168.200.19', user='ocdkube', token=token)
     launch_training_job("test", autogenerate=True, environ=env.external, 
-            framework=Framework.Tensorflow, workspace='mnist', script='python model.py',
+            workspace='mnist', script='python model.py',
             datasets=['mnist'])
     #export_model("/tmp/test-model", "test", autogenerate=True, environ=env.external, framework=Framework.Unknown)

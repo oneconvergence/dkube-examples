5918277a8305669b03b5b83c19c1a3e6152a652d
mak-454 <ahmed.khan@oneconvergence.com>
2019-10-15 07:38:55 -0700

    Modifications to run mnist pipeline example.

--- mnist-pl.py
+++ mnist-pl.py
@@ -2,27 +2,31 @@ import kfp.dsl as dsl
 from kfp import components
 import kfp.compiler as compiler
 
-import sys
-sys.path.insert(0, '/home/dkube/ahmed/oc-dkube-sdk/dkube/')
-
 from dkube.sdk.dkube import *
 from dkube.pipelines import *
 from dkube.sdk import *
 
 @dsl.pipeline(
-    name='dkube-mnist-pl',
+    name='dkube_mnist_pipeline',
     description='sample mnist digits pipeline with dkube components'
 )
-def d3pipeline():
-    token = 'eyJTZXNzaW9uIjp0cnVlLCJUb2tlbiI6ImV5SmhiR2NpT2lKSVV6STFOaUlzSW5SNWNDSTZJa3BYVkNKOS5leUpqY21WaGRHVmtJam94TlRjd01qVTNNakE1TENKeWIyeGxJam9pYjNCbGNtRjBiM0lpTENKMWMyVnlibUZ0WlNJNkltOWpaR3QxWW1VaWZRLlBsYjZMNEhDME16ZDlPLTItS1hrQVdyVGRyV2xCaGM1bGEyVEhCWk1kejQifQ=='
-    env = Environment(ip='192.168.200.19', user='ocdkube', token=token)
+def dkube_mnist_pipeline(container=ContainerImage.DKUBE_DS_TF_CPU_1_14.value.to_dict(),
+        token=os.getenv("DKUBE_USER_ACCESS_TOKEN"),
+        program='mnist',
+        dataset=['mnist'],
+        envs=[{"steps": 100, "batchsize": 100}],
+        hptuning={},
+        ngpus=0):
+
+    env = Environment(token='%s'%(token))
 
     train   = dkube_training_op(
             env=env.external, name='mnist-new', 
-            container=ContainerImage.DKUBE_DS_TF_CPU_1_14,
+            container=container,
             script='python model.py',
-            envs={'steps': 100, 'batchsize': 100},
-            program='mnist',
-            datasets=['mnist'])
+            envs=envs,
+            program=program,
+            datasets=dataset,
+            ngpus=ngpus)
 
-compiler.Compiler().compile(d3pipeline, 'dkube_mnist_pl.tar.gz')
+compiler.Compiler().compile(dkube_mnist_pipeline, 'dkube_mnist_pipeline.tar.gz')

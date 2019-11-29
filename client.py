1035a3536dccbb2affb1947d5ef9d75e7fc7d52c
mak-454 <ahmed.khan@oneconvergence.com>
2019-11-29 12:10:29 +0000

    fixed the exceptions in decoding version info, pass scheme=https to access from outside

--- client.py
+++ client.py
@@ -6,8 +6,8 @@ from dkube.sdk.dkube import *
 import os
 
 if __name__ == "__main__":
-    token = 'eyJTZXNzaW9uIjp0cnVlLCJUb2tlbiI6ImV5SmhiR2NpT2lKSVV6STFOaUlzSW5SNWNDSTZJa3BYVkNKOS5leUpqY21WaGRHVmtJam94TlRjd01qVTNNakE1TENKeWIyeGxJam9pYjNCbGNtRjBiM0lpTENKMWMyVnlibUZ0WlNJNkltOWpaR3QxWW1VaWZRLlBsYjZMNEhDME16ZDlPLTItS1hrQVdyVGRyV2xCaGM1bGEyVEhCWk1kejQifQ=='
-    env = Environment(host='192.168.200.19', user='ocdkube', token=token)
+    token = 'eyJTZXNzaW9uIjp0cnVlLCJUb2tlbiI6ImV5SmhiR2NpT2lKSVV6STFOaUlzSW5SNWNDSTZJa3BYVkNKOS5leUpqY21WaGRHVmtJam94TlRjek1EVXdNVFV6TENKeWIyeGxJam9pYjNCbGNtRjBiM0lpTENKMWMyVnlibUZ0WlNJNkltOWpaR3QxWW1VaWZRLlBGRFJTcC1UbXJNeUJRMXRLWm1EX0FmaHFCMVVOZkYtQW9wbjBVTjVFUXcifQ=='
+    env = Environment(scheme='https', host='18.236.126.102', user='ocdkube', token=token, port=32222)
     launch_training_job("test", autogenerate=True, environ=env.external, 
             workspace='mnist', script='python model.py',
             datasets=['mnist'])

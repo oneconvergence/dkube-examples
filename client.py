6fa9c6954d90573c96eb898c89f58e1454ca04d6
mak-454 <ahmed.khan@oneconvergence.com>
2020-01-21 03:26:41 -0800

    modified code to support new job apis

--- client.py
+++ client.py
@@ -1,14 +1,17 @@
 import sys
 
-sys.path.insert(0, '/home/dkube/sdk/dkube/')
+sys.path.insert(0, '/home/dkube/ahmed/dkube-sdk/dkube')
 
 from dkube.sdk.dkube import *
 import os
 
 if __name__ == "__main__":
-    token = 'eyJTZXNzaW9uIjp0cnVlLCJUb2tlbiI6ImV5SmhiR2NpT2lKSVV6STFOaUlzSW5SNWNDSTZJa3BYVkNKOS5leUpqY21WaGRHVmtJam94TlRjek1EVXdNVFV6TENKeWIyeGxJam9pYjNCbGNtRjBiM0lpTENKMWMyVnlibUZ0WlNJNkltOWpaR3QxWW1VaWZRLlBGRFJTcC1UbXJNeUJRMXRLWm1EX0FmaHFCMVVOZkYtQW9wbjBVTjVFUXcifQ=='
-    env = Environment(scheme='https', host='18.236.126.102', user='ocdkube', token=token, port=32222)
+    token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVhdGVkIjoxNTc4Mzg4MjAyLCJyb2xlIjoib3BlcmF0b3IiLCJ1c2VybmFtZSI6Im9jIn0.uAStfXdrojBBE_vx8Uyow1Coj1lQdG3mAzGBWbF4z_0'
+    env = Environment(scheme='https', host='34.83.166.110', user='oc', token=token, port=32222)
+    
     launch_training_job("test", autogenerate=True, environ=env.external, 
-            workspace='mnist', script='python model.py',
-            datasets=['mnist'])
+            workspace='mnist-1', script='python model.py',
+            datasets=['mnist'], models=['mnist'], template='test-7976809512')
+    
+    
     #export_model("/tmp/test-model", "test", autogenerate=True, environ=env.external, framework=Framework.Unknown)

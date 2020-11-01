1352d7365920009d96bf7e702c6a2e8a3101f15d
mak-454 <ahmed.khan@oneconvergence.com>
2020-11-02 01:13:37 +0530

    fixed pipeline code.

--- mnist-pl.py
+++ mnist-pl.py
@@ -10,19 +10,22 @@ from dkube.pipelines import *
     description='sample mnist digits pipeline with dkube components'
 )
 def dkube_mnist_pipeline(
-		authtoken='',
-		project='',
-		dataset='',
-		model='',
+		authtoken=None,
+		project=None,
+		dataset=None,
+		model=None,
+		script='python model.py',
 		nworkers=0,
 		ngpus=0):
 
     training_name= generate('mnist')
-    training = DkubeTraining('oc', name=training_name)
+    training = DkubeTraining('mak-454', name=training_name, description='triggered from dkube pl launcher')
+    training.update_container(framework="tensorflow_v1.14", image_url="ocdr/d3-datascience-tf-cpu:v1.14")
+    training.update_startupscript(str(script))
     training.add_project(str(project))
     training.add_input_dataset(str(dataset), mountpath='/opt/dkube/input')
     training.add_output_model(str(model), mountpath='/opt/dkube/output')
-	
+
     training_op = dkube_training_op(
 	            name='mnist-training',
 				authtoken=authtoken,

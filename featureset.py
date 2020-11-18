fb3992bab418d3c10791b38dedd0b5478a89671c
songole <songole@oneconvergence.com>
2020-11-17 23:59:21 -0800

    tested commit api

--- featureset.py
+++ featureset.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     user = 'osm'
     dkubeURL = 'https://172.16.146.128:32222'
-    authToken = 'eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc0YmNkZjBmZWJmNDRiOGRhZGQxZWIyOGM2MjhkYWYxIn0.eyJ1c2VybmFtZSI6Im9zbSIsInJvbGUiOiJkYXRhc2NpZW50aXN0LG1sZSxwZSxvcGVyYXRvciIsImV4cCI6NDg0NTQwNjcxNywiaWF0IjoxNjA1NDA2NzE3LCJpc3MiOiJES3ViZSJ9.0sb9UTU8DDv42Wml4GQP6tdTPLOpkHbED8aw-cRf1uGBKfW7cD5TNNVckkVAEYgwZrpdBeDFj3vnhn_GLJMjxzC9rDlaIE50Aka1OLOiaN5_qvpBbKdTqiIglFLmIZSj9mESCfnqQtP2o2FKdJHdJo6Zeawb5-IBFZu3dMIgrdDtPlIwV40ICqUjH5S-3D0UtaEkcNRyO0h2BGbhro8jHfX88MYYmMurXhRJs2BgnvP_6Or0_mheuBsClTXbM3m62Oc2Mqml0gk23_qEqdmcPv3i9wF0tS3AM4vTVbN-gV7SrLaRph93mntD6NFv9AX7wcVIs1qxY1DfsstvRPYawQ'
+    authToken = 'eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc0YmNkZjBmZWJmNDRiOGRhZGQxZWIyOGM2MjhkYWYxIn0.eyJ1c2VybmFtZSI6Im9zbSIsInJvbGUiOiJkYXRhc2NpZW50aXN0LG1sZSxwZSxvcGVyYXRvciIsImV4cCI6NDg0NTY0NDMyMywiaWF0IjoxNjA1NjQ0MzIzLCJpc3MiOiJES3ViZSJ9.xMd-OpVe4-RdVClyvzX4bhibWNNB4nDI9WY4qbMGyYyJzrUMjb4xa-6hf0EbOXUh7ePYjKwFyuJJa-tXTj-L_pF_QlyycKF66WPIAl6-soMFIhmZXWlGCUqXHF9-pUkBqlpLLCwz07Z4nUzksMPkoIbHwWf_MUNll-lbaCNgd-z_X97ADu6lTBfhm3bV2eXnDBV5oBoxjF0Fg4fJxl_dNBzw926lswaWwndElr3VyDtpxV5hcsVKFFufunIn6r4leP6K_fYnT6E449IWOaAxYoxyTkmhQDkHBAJ3Odpd19wry1xgi9erRHB059oSr8wP3JqcPqP6hRTdpIEA9id03g'
 
     featureset_name = generate('elections-2020')
     print(f"Featureset name {featureset_name}")
@@ -58,6 +58,7 @@ if __name__ == "__main__":
     print(response)
     """
 
+    """
     # Create a dataset
 
     dataset_name = generate('mnist')
@@ -69,12 +70,13 @@ if __name__ == "__main__":
 
     # Create a project
 
+    
     project_name = generate('mnist')
     project = DkubeProject(user, name=project_name)
     project.update_git_details('https://github.com/oneconvergence/dkube-examples/tree/2.0.6/tensorflow/classification/mnist/digits/classifier/program', branch='2.0.6')
 
     api.create_project(project)
-
+    
     # Create model repo
 
     model_name = generate('mnist')
@@ -85,11 +87,14 @@ if __name__ == "__main__":
 
     """
     # Preprocessing run
-    preprocess_name= generate('mnist-preprocess')
+    project_name = "mnist-fs"
+    dataset_name = "mnist-fs"
+    featureset_name = "A"
+    preprocess_name= generate('mnist-fs')
     preprocess = DkubePreprocessing(user, name=preprocess_name, description='triggered from dkube sdk')
     preprocess.update_container(image_url="ocdr/d3-datascience-tf-cpu:v1.14")
-    preprocess.update_startupscript("sleep 2000")
-    #preprocess.add_project(project_name)
+    preprocess.update_startupscript("sleep 200000")
+    preprocess.add_project(project_name)
     preprocess.add_input_dataset(dataset_name, mountpath='/opt/dkube/input')
     preprocess.add_output_featureset(featureset_name, mountpath='/opt/dkube/output')
 
@@ -108,4 +113,4 @@ if __name__ == "__main__":
     
 
     api.create_training_run(training)
-
+    """

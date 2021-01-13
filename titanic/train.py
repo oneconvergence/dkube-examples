74f6aae2fcbf621521e0ccea64cc09960d334467
Rajendra <rajendra.singh@oneconvergence.com>
2021-01-12 20:17:51 -0600

    removed mnist. added titanic sklearn example

--- titanic/train.py
+++ titanic/train.py
@@ -1,53 +1,106 @@
-import pyarrow as pa
-import pyarrow.parquet as pq
-import numpy as np
-import requests, os
-import argparse
-import joblib
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from sklearn.ensemble import RandomForestClassifier
-import mlflow
-from dkube.sdk import *
+from sklearn.metrics import make_scorer, accuracy_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from mlflow import log_metric
+import joblib
+import os
 
-inp_path = "/featureset/train"
-out_path = "/model"
-test_path = "/featureset/test"
+train = pd.read_csv("/titanic-train/train.csv")
+test = pd.read_csv("/titanic-test/test.csv")
 
+def encode_features(df_train, df_test): #label encoding for sklearn algorithms
+    features = ['Fare', 'Cabin', 'Age', 'Sex', 'Lname', 'NamePrefix']
+    df_combined = pd.concat([df_train[features], df_test[features]])
+    for feature in features:
+        le = preprocessing.LabelEncoder()
+        le = le.fit(df_combined[feature])
+        df_train[feature] = le.transform(df_train[feature])
+        df_test[feature] = le.transform(df_test[feature])
+    return df_train, df_test
 
-if __name__ == "__main__":
+def simplify_ages(df): # Binning ages 
+    df.Age = df.Age.fillna(-0.5)
+    bins = (-1, 0, 5, 12, 18, 25, 35, 60, 120)
+    group_names = ['Unknown', 'Baby', 'Child', 'Teenager', 'Student', 'Young Adult', 'Adult', 'Senior']
+    categories = pd.cut(df.Age, bins, labels=group_names)
+    df.Age = categories
+    return df
 
-    ########--- Read features from input FeatureSet ---########
+def simplify_cabins(df): # Storing first letter of cabins
+    df.Cabin = df.Cabin.fillna('N')
+    df.Cabin = df.Cabin.apply(lambda x: x[0])
+    return df
 
-    # Featureset API
-    featureset = DkubeFeatureSet()
-    # Specify featureset path
-    featureset.update_features_path(path=inp_path)
+def simplify_fares(df): # Binning fares
+    df.Fare = df.Fare.fillna(-0.5)
+    bins = (-1, 0, 8, 15, 31, 1000)
+    group_names = ['Unknown', '1_quartile', '2_quartile', '3_quartile', '4_quartile']
+    categories = pd.cut(df.Fare, bins, labels=group_names)
+    df.Fare = categories
+    return df
 
-    # Read features
-    data = featureset.read()  # output: response json with data
-    feature_df = data["data"]
-    train, val = train_test_split(feature_df, test_size=0.2)
-    ########--- Train ---########
+def format_name(df): # Keeping title
+    df['Lname'] = df.Name.apply(lambda x: x.split(' ')[0])
+    df['NamePrefix'] = df.Name.apply(lambda x: x.split(' ')[1])
+    return df    
+    
+def drop_features(df): # Dropping useless features
+    return df.drop(['Ticket', 'Name', 'Embarked'], axis=1)
 
-    # preparing input output pairs
-    y = train["Survived"].values
-    x = train.drop(["PassengerId","Survived"], 1).values
+def transform_features(df):
+    df = simplify_ages(df)
+    df = simplify_cabins(df)
+    df = simplify_fares(df)
+    df = format_name(df)
+    df = drop_features(df)
+    return df
 
-    # Training random forest classifier
-    model_RFC = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
-    model_RFC.fit(x, y)
-    predictions = model_RFC.predict(x)
+train = transform_features(train)
+test = transform_features(test)
+train_vis = train.copy(deep=True) # data for visualization
+train, test = encode_features(train, test)
+train.head()
 
-    ########--- Log metrics to DKube ---########
+X_all = train.drop(['Survived', 'PassengerId'], axis=1)
+y_all = train['Survived']
 
-    # Calculating accuracy
-    accuracy = accuracy_score(y, predictions)
-    # logging acuracy to DKube
-    mlflow.log_metric("accuracy", accuracy)
+num_test = 0.20
+X_train, X_test, y_train, y_test = train_test_split(X_all, y_all, test_size=num_test, random_state=23)
 
-    ########--- Write model to DKube ---########
+# Choose the type of classifier. 
+clf = RandomForestClassifier()
 
-    # Exporting model
-    filename = os.path.join(out_path, "model.joblib")
-    joblib.dump(model_RFC, filename)
+# Choose some parameter combinations to try
+parameters = {'n_estimators': [4, 6, 9], 
+              'max_features': ['log2', 'sqrt','auto'], 
+              'criterion': ['entropy', 'gini'],
+              'max_depth': [2, 3, 5, 10], 
+              'min_samples_split': [2, 3, 5],
+              'min_samples_leaf': [1,5,8]
+             }
+
+# Type of scoring used to compare parameter combinations
+acc_scorer = make_scorer(accuracy_score)
+
+# Run the grid search
+grid_obj = GridSearchCV(clf, parameters, scoring=acc_scorer)
+grid_obj = grid_obj.fit(X_train, y_train)
+
+# Set the clf to the best combination of parameters
+clf = grid_obj.best_estimator_
+
+# Fit the best algorithm to the data. 
+clf.fit(X_train, y_train)
+
+predictions = clf.predict(X_test)
+accuracy = accuracy_score(y_test, predictions)
+print("accuracy", accuracy)
+
+log_metric("accuracy", accuracy)
+
+os.makedirs("/model/1/", exist_ok=True)
+joblib.dump(clf, "/model/1/model.joblib")

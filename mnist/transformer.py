c472e8120ee2fd3942d5505ddefd19bce675508d
Rajendra <rajendra.singh@oneconvergence.com>
2021-01-13 11:06:15 -0600

    fixed transformer script

--- mnist/transformer.py
+++ mnist/transformer.py
@@ -1,17 +1,13 @@
-import kfserving
 from typing import List, Dict
-from PIL import Image
+import kfserving
+import json
 import logging
-import io
-import numpy as np
-import base64
 import argparse
-
-import sys,json
+import numpy as np
 import requests
-import os
-import logging
 import cv2
+import pandas as pd
+import base64
 
 DEFAULT_MODEL_NAME = "model"
 
@@ -22,46 +18,32 @@ parser.add_argument('--predictor_host', help='The URL for the model predict func
 
 args, _ = parser.parse_known_args()
 
-filename = 'temp.jpg'
-img_w = 28
-img_h = 28
-
-
-def b64_filewriter(filename, content):
-    string = content.encode('utf8')
-    b64_decode = base64.decodebytes(string)
-    fp = open(filename, "wb")
-    fp.write(b64_decode)
-    fp.close()
-
-
-class ImageTransformer(kfserving.KFModel):
+class Transformer(kfserving.KFModel):
     def __init__(self, name: str, predictor_host: str):
         super().__init__(name)
         self.predictor_host = predictor_host
 
     def preprocess(self, inputs: Dict) -> Dict:
+        logging.info("inputs %s", str(inputs))
         del inputs['instances']
-        print (inputs)
         try:
             json_data = inputs
         except ValueError:
             return json.dumps({ "error": "Recieved invalid json" })
-        data = json_data["signatures"]["inputs"][0][0]["data"]
-        b64_filewriter(filename, data)
-        img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
-        x = img
-        x = cv2.resize(x, (img_w, img_h))
-        x = np.array(x, dtype=np.float64)
-        x = x.reshape(1,img_h,img_w,1)
-        payload = {"inputs": {'input_1': x.tolist()}, 'token':inputs['token']}
-        print (payload)
+        data = json_data["signatures"]["inputs"][0][0]["data"].encode()
+        with open("image.png", "wb") as fh:
+            fh.write(base64.decodebytes(data))
+        img = cv2.imread("image.png", cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (28,28))
+        img = img.reshape(1,1,img.shape[0],img.shape[1])
+        img = img.astype('float32')
+        payload = {"instances": img.tolist(), "token":inputs["token"]}
         return payload
 
     def postprocess(self, inputs: List) -> List:
         return inputs
 
 if __name__ == "__main__":
-    transformer = ImageTransformer(args.model_name, predictor_host=args.predictor_host)
+    transformer = Transformer(args.model_name, predictor_host=args.predictor_host)
     kfserver = kfserving.KFServer()
     kfserver.start(models=[transformer])

8307077e927ff65b26f47459535de34eadab38b5
Rajendra <rajendra.singh@oneconvergence.com>
2021-01-24 16:52:26 -0600

    use updated featureset API

--- titanic/predict.py
+++ titanic/predict.py
@@ -8,18 +8,14 @@ model_dir = "/model"
 test_fs_dir= "/test_fs"
 
 def predict():
-    featureset = DkubeFeatureSet()
-    featureset.update_features_path(path=test_fs_dir)
     os.system ("ls -l " + test_fs_dir)
-    test_df = featureset.read()["data"]
-    print (test_df.columns)
-    testdf_tmp = test_df
-    df = testdf_tmp.drop("PassengerId", 1)
+    df = DkubeFeatureSet.read_features(test_fs_dir)
+    print (df.columns)
     #df = testdf_tmp.drop(["PassengerId","Survived"], 1)
     df = pd.DataFrame(df).fillna(df.mean())
     model = joblib.load(os.path.join(model_dir, "model.joblib"))
     predictions = model.predict(df)
-    output = pd.DataFrame({'PassengerId': test_df.PassengerId, 'Survived': predictions})
+    output = pd.DataFrame({'PassengerId': df.PassengerId, 'Survived': predictions})
     output.to_csv("/tmp/prediction.csv", index=False)
     print("predictions generated.")
 

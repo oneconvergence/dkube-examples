7f6ee59522f36f3222b3a4b0ac2f874b5b1fc224
Rajendra <rajendra.singh@oneconvergence.com>
2021-02-06 19:42:59 -0600

    fixes in prediction script

--- titanic/predict.py
+++ titanic/predict.py
@@ -9,13 +9,13 @@ test_fs_dir= "/test_fs"
 
 def predict():
     os.system ("ls -l " + test_fs_dir)
-    df = DkubeFeatureSet.read_features(test_fs_dir)
-    print (df.columns)
-    #df = testdf_tmp.drop(["PassengerId","Survived"], 1)
+    test_df = DkubeFeatureSet.read_features(test_fs_dir)
+    print (test_df.columns)
+    df = test_df.drop(["PassengerId"], 1)
     df = pd.DataFrame(df).fillna(df.mean())
     model = joblib.load(os.path.join(model_dir, "model.joblib"))
     predictions = model.predict(df)
-    output = pd.DataFrame({'PassengerId': df.PassengerId, 'Survived': predictions})
+    output = pd.DataFrame({'PassengerId': test_df.PassengerId, 'Survived': predictions})
     output.to_csv("/tmp/prediction.csv", index=False)
     print("predictions generated.")
 

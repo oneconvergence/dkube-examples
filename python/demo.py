35b8c60e945953ba34fbf5078fe7c52c6d2e4fbd
riteshkarvaloc <ritesh.k@oneconvergence.com>
2021-04-01 14:10:19 +0000

    fixes

--- python/demo.py
+++ python/demo.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
 
     authToken = os.getenv('DKUBE_USER_ACCESS_TOKEN')
     user = os.getenv('USERNAME', 'oc')
-
+    api = DkubeApi(token=authToken)
+    
     code_name = generate('mnist')
     code = DkubeCode(user, name=code_name)
     code.update_git_details('https://github.com/oneconvergence/dkubeio-examples/tree/master/tf/classification/mnist/digits/classifier/program')
@@ -28,70 +29,80 @@ if __name__ == "__main__":
     training = DkubeTraining(user, name=training_name, description='triggered from dkube sdk')
     training.update_container(framework="tensorflow_1.14", image_url="ocdr/d3-datascience-tf-cpu:v1.14")
     training.update_startupscript("python model.py")
-    training.add_code("mnist")
-    training.add_input_dataset("mnist", mountpath='/opt/dkube/input')
-    training.add_output_model("mnist", mountpath='/opt/dkube/output')
+    training.add_code(code_name)
+    training.add_input_dataset(dataset_name, mountpath='/opt/dkube/input')
+    training.add_output_model(model_name, mountpath='/opt/dkube/output')
 
-    serving_name=geneirate('mnist')
+    serving_name=generate('mnist')
     serving = DkubeServing(user, name=serving_name, description='serving deployed from dkube sdk')
-    serving.set_transformer(True, script='tensorflow/classification/mnist/digits/transformer/transformer.py')
-    serving.update_serving_model("mnist")
+    serving.set_transformer(True, script='tf/classification/mnist/digits/transformer/transformer.py')
+    serving.update_serving_model(model_name)
 
-    notebook_name= generate('mnist')
-    notebook = DkubeIDE(user, name=notebook_name, description='triggered from dkube sdk')
-    notebook.update_container(framework="tensorflow_1.14", image_url="ocdr/dkube-datascience-rs-tf-cpu-multiuser:v1.14")
-
+    jl_notebook_name= generate('mnist')
+    jl_notebook = DkubeIDE(user, name=jl_notebook_name, description='triggered from dkube sdk')
+    jl_notebook.update_container(framework="tensorflow_1.14", image_url="ocdr/d3-datascience-tf-cpu-multiuser:v1.14-4")
+    
+    rs_notebook_name= generate('mnist')
+    rs_notebook = DkubeIDE(user, name=rs_notebook_name, description='triggered from dkube sdk')
+    rs_notebook.update_container(framework="tensorflow_1.14", image_url="ocdr/dkube-datascience-rs-tf-cpu-multiuser:v1.14-4")
+    
+    api.create_code(code)
+    api.create_dataset(dataset)
+    api.create_model(model)
+    api.launch_jupyter_ide(jl_notebook)
+    api.launch_rstudio_ide(rs_notebook)
+    print(api.list_ides(user))
+    api.create_training_run(training)
+    
+    for each_datum in api.list_models(user = user)[0]["datums"]:
+        if each_datum["datum"]["name"] == model_name:
+            head_version = each_datum["datum"]["generated"]["head_version"]
+            break
     publish_name=generate('mnist')
     publish_details = DkubeServing(user, name=publish_name, description='model published from dkube sdk')
     publish_details.set_transformer(True, script='tensorflow/classification/mnist/digits/transformer/transformer.py')
-    publish_details.update_serving_model("mnist", version='1604346481344')
+    publish_details.update_serving_model(model_name, version=head_version)
 
+    
+    
+    api.publish_model(publish_name, "publishing mnist model", publish_details)
+    # model(user, model_name, version=head_version)
 
-#     api = DkubeApi(token=authToken)
-#     api.launch_jupyter_ide(notebook)
-#     api.launch_rstudio_ide(notebook)
-#     print(api.list_ides(user))
-#     api.delete_ide(user, 'mnist-4674')
-#     api.create_code(code)
-#     api.create_dataset(dataset)
-#     api.create_model(model)
-#     v = api.get_model_version(user, "mnist", "1604347322339")
-#     api.publish_model(publish_name, "publishing mnist model", publish_details)
-#     api.release_model(user, "mnist", "1604345493555")
+    mc = api.modelcatalog(user)
+    #item = api.get_modelcatalog_item(user, model_name, head_version)
+    item = api.get_modelcatalog_item(user, model=publish_name, version=head_version)
+    api.create_model_deployment(user, 'dep1', publish_name, head_version, stage_or_deploy='deploy')
+    deps = api.list_model_deployments(user)
 
-#     mc = api.modelcatalog(user)
-#     item = api.get_modelcatalog_item(user, 'mnist-7206', '1606471424577')
-#     api.create_model_deployment(user, 'dep1', 'mnist-7206', '1606471424577', stage_or_deploy='deploy')
-#     api.delete_model_deployment(user, 'dep1')
-#     deps = api.list_model_deployments(user)
+    api.create_test_inference(serving)
 
-#     api.create_training_run(training)
+    lineage = api.get_model_lineage(user, model_name, head_version)
+    lineage = api.get_training_run_lineage(user, training_name)
+    versions = api.get_dataset_versions(user, dataset_name)
+    versions = api.get_model_versions(user, model_name)
 
-#     api.create_test_inference(serving)
+    version = api.get_model_latest_version(user, model_name)
+    version = api.get_dataset_latest_version(user, dataset_name)
 
-#     lineage = api.get_model_lineage(user, 'mnist', '1604322708542')
-#     lineage = api.get_training_run_lineage(user, 'mnist-4990')
-#     versions = api.get_dataset_versions(user, 'mnist')
-#     versions = api.get_model_versions(user, 'mnist')
-
-#     version = api.get_model_latest_version(user, 'mnist')
-#     version = api.get_dataset_latest_version(user, 'mnist')
-
-#     cap = api.get_datascience_capabilities()
-#     print(api.get_notebook_capabilities())
-#     print(api.get_r_capabilities())
-#     print(api.get_training_capabilities())
-#     print(api.get_serving_capabilities())
+    cap = api.get_datascience_capabilities()
+    print(api.get_notebook_capabilities())
+    print(api.get_r_capabilities())
+    print(api.get_training_capabilities())
+    print(api.get_serving_capabilities())
+    
+    api.delete_ide(user, jl_notebook_name)
+    api.delete_ide(user, rs_notebook_name)
+    api.delete_model_deployment(user, 'dep1')
 
     '''
-    reruns = api.trigger_runs_bydataset("mnist", "oc")
+    reruns = api.trigger_runs_bydataset(dataset_name, user)
     import json
     print('List of reruns - {}'.format(json.dumps(reruns)))
     '''
 
     '''
     training_name = 'pltraining-3500'
-    outputs = api.get_training_outputs('oc', training_name)
+    outputs = api.get_training_outputs(user, training_name)
 
     artifacts = [
                     {'datum': output['version']['datum_name'], 'class': output['version']['datum_type'],

01f3a24294dd288bbe3a9e3feecc6a89f76e1616
Rahul Malhotra <53820555+rahul-179@users.noreply.github.com>
2021-09-13 11:04:04 +0530

    Update volumes-and-secret.py

--- volumes-and-secret/volumes-and-secret.py
+++ volumes-and-secret/volumes-and-secret.py
@@ -6,7 +6,7 @@ from kfp import components, dsl
     base_image="ocdr/dkube-datascience-tf-cpu:v2.0.0-6" 
 )
 def print_secret(user):
-    '''Calculates sum of two arguments'''
+    '''Displays access-token secret'''
     from kubernetes import client, config
     config.load_incluster_config()
     v1=client.CoreV1Api()
@@ -15,7 +15,7 @@ def print_secret(user):
 
 print_secret_op = components.func_to_container_op(
     print_secret,
-    base_image="ocdr/dkube-datascience-tf-cpu:v2.0.0-6", 
+    base_image="datadog/agent:latest", 
 )
 
 @dsl.pipeline(
@@ -23,9 +23,10 @@ print_secret_op = components.func_to_container_op(
     description='sample pipeline to display usage of volume and secrets'
 )
 def volumes_and_secret(username):
+    """ Default storage class must be available in the cluster for pipelineVolume to work."""
     volume = dsl.PipelineVolume(volume=kubernetes.client.V1Volume(
         name=f"test-storage",
         empty_dir=kubernetes.client.V1EmptyDirVolumeSource()))
     mount_folder = "/tmp"
-    print_secret = print_secret_op(str(username)).add_pvolumes({mount_folder: volume})
-    _ = dsl.ContainerOp(name="print_volume", image="bash:latest", command=['df', '-h']).add_pvolumes({mount_folder: volume}).after(print_secret)
+    print_volume = dsl.ContainerOp(name="print_volume", image="bash:latest", command=['df', '-h']).add_pvolumes({mount_folder: volume})
+    _ = print_secret_op(str(username)).after(print_volume)

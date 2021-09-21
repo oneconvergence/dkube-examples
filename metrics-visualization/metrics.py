4c8584aa3e2951c961b6fafed74940e3d4c97ac0
rahul-179 <rahul.malhotra@oneconvergence.com>
2021-09-22 04:01:17 +0530

    update 2

--- metrics-visualization/metrics.py
+++ metrics-visualization/metrics.py
@@ -10,6 +10,10 @@ def produce_metrics():
     ]
 
     df = pd.DataFrame(matrix,columns=['target','predicted','count'])
+    df_roc = pd.DataFrame({'fpr': fpr, 'tpr': tpr, 'thresholds': thresholds})
+    roc_file = '/output/roc.csv'
+    with open(roc_file, 'w') as f:
+        df_roc.to_csv(f, columns=['fpr', 'tpr', 'thresholds'], header=False, index=False)
 
     metadata = {
         "outputs": [
@@ -36,12 +40,40 @@ def produce_metrics():
                     "yummy",
                     "not yummy"
                 ]
+            },
+            {
+            'type': 'roc',
+            'format': 'csv',
+            'schema': [
+                {'name': 'fpr', 'type': 'NUMBER'},
+                {'name': 'tpr', 'type': 'NUMBER'},
+                {'name': 'thresholds', 'type': 'NUMBER'},
+            ],
+            'source': roc_file
             }
         ]
     }
 
     with open("/output/metrics.json", "w") as f:
         json.dump(metadata, f)
+    accuracy_score = 0.6
+    roc_auc_score = 0.75
+    metrics = {
+        'metrics': [
+            {
+                'name': 'accuracy-score',
+                'numberValue':  accuracy_score,
+                'format': 'PERCENTAGE'
+            },
+            {
+                'name': 'roc-auc-score',
+                'numberValue':  roc_auc_score,
+                'format': 'RAW'       
+            }
+        ]
+    }
+    with open('/output/mlpipeline-metrics.json', 'w') as f:
+        json.dump(metrics, f)
 
 
 produce_metrics()

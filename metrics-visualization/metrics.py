ea1663f2b803cf951b4a3ea2971871b7ad260e99
rahul-179 <rahul.malhotra@oneconvergence.com>
2021-10-04 09:29:21 +0530

    Update metrics visualization

--- metrics-visualization/metrics.py
+++ metrics-visualization/metrics.py
@@ -73,6 +73,16 @@ def produce_metrics():
 
     with open("/output/metrics.json", "w") as f:
         json.dump(metadata, f)
+    accuracy = 70
+    metrics = {
+    'metrics': [{
+      'name': 'accuracy-score', # The name of the metric. Visualized as the column name in the runs table.
+      'numberValue':  accuracy, # The value of the metric. Must be a numeric value.
+      'format': "PERCENTAGE",   # The optional format of the metric. Supported values are "RAW" (displayed in raw format) and "PERCENTAGE" (displayed in percentage format).
+    }]
+  }
+    with open("/output/mlpipeline-metrics.json", "w") as f:
+        json.dump(metadata, f)
 
 
 produce_metrics()

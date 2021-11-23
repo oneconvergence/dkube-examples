7bdc670ebfc7f129baa87824ff709f49010b05c4
pallavi-pannu-oc <67274173+pallavi-pannu-oc@users.noreply.github.com>
2021-11-23 16:29:44 +0530

    adding titanic-eg

--- titanic/training.py
+++ titanic/training.py
@@ -1,6 +1,8 @@
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 from tensorflow import keras
+from keras import backend as K
+from sklearn.model_selection import train_test_split
 import tensorflow as tf
 import transform_data
 from sklearn import preprocessing
@@ -12,7 +14,7 @@ import pandas as pd
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--batch_size', type=int, default=1, help='Batch size for training.')
-parser.add_argument('--num_epochs', type=int, default=int(os.getenv("EPOCHS","20")), help='Number of epochs to train for.')
+parser.add_argument('--num_epochs', type=int, default=int(os.getenv("EPOCHS","5")), help='Number of epochs to train for.')
 parser.add_argument('--data_source',type=str,default='local',help='Data source')
 args = parser.parse_args()
 
@@ -24,6 +26,24 @@ print ("Number of epochs:", epochs)
 train_path = "/train-data"
 MODEL_DIR = "/model/"
 
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+
+
 ## loading dataset
 train = pd.read_csv(train_path+'/data.csv')
 if data_source == 'local' or data_source == 'aws_s3':
@@ -32,30 +52,25 @@ if data_source == 'local' or data_source == 'aws_s3':
     train = transformer.preprocess(train)
 y_train = train["Survived"].values
 x_train = train.drop(['Survived','PassengerId','timestamp'],axis=1).values
+X_train, X_test, Y_train, Y_test = train_test_split(x_train, y_train, test_size=0.3, random_state=512)
 
 # Network
 model = Sequential()
 model.add(Dense(12, activation='relu', input_shape=(7,)))
 model.add(Dense(8, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc',f1_m,precision_m, recall_m])
 
-# mlflow metric logging
-class loggingCallback(keras.callbacks.Callback):
-    def on_epoch_end(self, epoch, logs=None):
-        accuracy_metric = "accuracy"
-        if "acc" in logs:
-            accuracy_metric = "acc"
+model.fit(X_train, Y_train, batch_size=batch_size, epochs=epochs, verbose=0, validation_split=0.1)
 
-        log_metric ("train_loss", logs["loss"], step=epoch)
-        log_metric ("train_accuracy", logs[accuracy_metric], step=epoch)
-        log_metric ("val_loss", logs["val_loss"], step=epoch)
-        log_metric ("val_accuracy", logs["val_" + accuracy_metric], step=epoch)
-        # output accuracy metric for katib to collect from stdout
-        print(f"accuracy={round(logs['val_' + accuracy_metric],2)}")
+# evaluate the model
+loss, accuracy, f1_score, precision, recall = model.evaluate(X_test, Y_test, verbose=0)
 
-model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=0, validation_split=0.1, 
-        callbacks=[loggingCallback(), tf.keras.callbacks.TensorBoard(log_dir=MODEL_DIR)])
+## logging the metrics
+log_metric("accuracy",accuracy)
+log_metric("f1_score",f1_score)
+log_metric("precision",precision)
+log_metric("recall",recall)
 
 model.save(MODEL_DIR + 'weights.h5')
 tf.keras.backend.set_learning_phase(0)  # Ignore dropout at inference

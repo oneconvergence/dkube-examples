2e3bde980f5cdf77b97e9fd76c92d9558123a530
deepio-oc <55089906+deepio-oc@users.noreply.github.com>
2022-03-25 10:57:20 -0500

    simulate health metrics (#89)

--- metrics/server/server/main.py
+++ metrics/server/server/main.py
@@ -4,6 +4,8 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from .schema import *
 import json
+import csv
+import time
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
@@ -12,6 +14,9 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 app = FastAPI()
 
+reader = csv.DictReader(open("server/metrics.csv"))
+metrics = [row for row in reader]
+index = 0
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
@@ -35,5 +40,10 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
 
 @app.post("/query_range", response_model=MetricResponse)
 async def get_metrics(request: MetricRangeRequest, current_user: str = Depends(get_current_user)):
-    metrics = json.load(open("server/metrics.json"))
-    return {"status": "success", "data": metrics}
+    global index
+    metric = metrics[index % len(metrics)]
+    index += 1
+    data = []
+    for k,v in metric.items():
+        data.append({"metric":{"name":k}, "values": [[time.time(), v]]})
+    return {"status": "success", "data": data}

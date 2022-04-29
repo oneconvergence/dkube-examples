3c2a1eef7be13841448bdee6c2ac52d91e05de0a
deepio-oc <55089906+deepio-oc@users.noreply.github.com>
2022-04-29 08:33:42 -0500

    Update main.py

--- metrics/server/server/main.py
+++ metrics/server/server/main.py
@@ -44,7 +44,7 @@ async def get_metrics(request: MetricRangeRequest, current_user: str = Depends(g
     metric = metrics[index % len(metrics)]
     index += 1
     data = []
-    timestamp = time.time()
+    timestamp = int(time.time())
     for k,v in metric.items():
         data.append({"metric":{"name":k}, "values": [[timestamp, v]]})
     return {"status": "success", "data": data}

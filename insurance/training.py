b356cd7d4f933995cfba65e950886c4a0bc43b1d
riteshkarvaloc <ritesh.k@oneconvergence.com>
2022-12-05 13:06:38 +0530

    DL model for insurance

--- insurance/training.py
+++ insurance/training.py
@@ -1,37 +1,27 @@
-from dkube.sdk import mlflow as dkubemlf
-
-import numpy as np,os
-import joblib
+import os
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn import linear_model
 from sklearn import preprocessing as skpreprocessing
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler
 import mlflow
+from mlflow.models.signature import infer_signature
 import pandas as pd
-from sklearn import metrics
-import joblib
 
-import requests, argparse
-requests.packages.urllib3.disable_warnings()
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
 import warnings
 warnings.filterwarnings("ignore")
+import requests, argparse
+requests.packages.urllib3.disable_warnings()
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--n_estimators', type=int, default=100,
-                        help='The number of trees in the forest.')
-parser.add_argument('--max_depth', type=int, default=None,
-                        help="The maximum depth of the tree.")
-parser.add_argument('--max_features', type=int, default=1,
-                        help='The number of maximum features provided to each tree in a random forest')
+parser.add_argument('--epochs', type=int, default=10,
+                        help='The number of epochs for training')
+parser.add_argument('--learning_rate', type=int, default=None,
+                        help="learning rate for optimizer")
 args = parser.parse_args()
-
-n_estimators = args.n_estimators
-max_depth = args.max_depth
-max_features = args.max_features
-
 
 # ### MACROS
 
@@ -41,7 +31,9 @@ MLFLOW_EXPERIMENT_NAME = os.getenv('DKUBE_PROJECT_NAME', 'insurance')
 # EPOCHS, DATASET_URL could be specified as Environment parameters at the time of creating JL or Run
 
 # Experiment with this parameter. 
-NUM_EPOCHS = int(os.getenv("EPOCHS", 10))
+NUM_EPOCHS = int(os.getenv("EPOCHS", args.epochs))
+
+LEARNING_RATE = args.learning_rate
 
 # Define data
 INPUT_DATA_URL = os.getenv("DATASET_URL", "https://dkube-examples-data.s3.us-west-2.amazonaws.com/monitoring-insurance/training-data/insurance.csv")
@@ -52,8 +44,7 @@ OUTPUT_MODEL_DIR = os.getcwd()+"/model"
 
 
 ## create OUTPUT_MODEL_DIR
-if not os.path.exists(OUTPUT_MODEL_DIR):
-    os.makedirs(OUTPUT_MODEL_DIR)
+os.makedirs(OUTPUT_MODEL_DIR, exist_ok=True)
 
 
 # #### MLFLOW TRACKING INITIALIZATION
@@ -84,49 +75,48 @@ for col in ['sex', 'smoker', 'region']:
 #standardize data
 x_scaled = StandardScaler().fit_transform(insurance_input)
 x_train, x_test, y_train, y_test = train_test_split(x_scaled,
-                                                    insurance_target,
+                                                    insurance_target.values,
                                                     test_size = 0.25,
                                                     random_state=1211)
 #fit random forest regressor to the train set data
-rfc = RandomForestRegressor(n_estimators = n_estimators,
-                           max_depth=max_depth,
-                           max_features=max_features)
+tf.random.set_seed(42)  #first we set random seed
+model = keras.Sequential([
+      layers.InputLayer(input_shape=(6)),
+      layers.Dense(64, activation='relu'),
+      layers.Dense(64, activation='relu'),
+      layers.Dense(1)
+  ])
 
-# #### ML TRAINING
+model.compile(loss='mean_absolute_error',
+            optimizer=tf.keras.optimizers.Adam(lr=LEARNING_RATE))
+
+
+# mlflow metric logging
+class loggingCallback(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        accuracy_metric = "loss"
+        mlflow.log_metric("train_loss", logs["loss"], step=epoch)
+        mlflow.log_metric("val_loss", logs["val_loss"], step=epoch)
+        # output accuracy metric for katib to collect from stdout
+        print(f"loss={round(logs['loss'],2)}")
 
 
 with mlflow.start_run(run_name="insurance") as run:
     
-    model = rfc.fit(x_train, y_train)
-    
-    y_pred_train = model.predict(x_train)    # Predict on train data.
-    y_pred_train[y_pred_train < 0] = y_pred_train.mean()
-    y_pred = model.predict(x_test)   # Predict on test data.
-    y_pred[y_pred < 0] = y_pred.mean()
-    
-    #######--- Calculating metrics ---############
-    mae = metrics.mean_absolute_error(y_test, y_pred)
-    mse = metrics.mean_squared_error(y_test, y_pred)
-    rmse = np.sqrt(metrics.mean_squared_error(y_test, y_pred))
-
-    ########--- Logging metrics into Dkube via mlflow ---############
-    mlflow.log_metric("MAE", mae)
-    mlflow.log_metric("MSE", mse)
-    mlflow.log_metric("RMSE", rmse)
-
-    print(f"mean_absolute_error={mae}")
+    model.fit(x_train, y_train, epochs = NUM_EPOCHS, validation_split=0.1,
+             callbacks=[loggingCallback()])
     
     # Exporting model
-    filename = os.path.join(OUTPUT_MODEL_DIR, "model.joblib")
-    joblib.dump(model, filename)
+    model.save(filepath=os.path.join(OUTPUT_MODEL_DIR, '1'))
     
     # Two ways to save model - log_artifacts() or log_model()
     #mlflow.log_artifacts(OUTPUT_MODEL_DIR, artifact_path="saved_model")
-    mlflow.sklearn.log_model(model, "saved_model")
-    
+    signature = infer_signature(x_test, model.predict(x_test))
+    mlflow.keras.log_model(model, "insurance_model", signature=signature)
+        
     # Record parameters
     mlflow.log_params({"dataset": "https://dkube-examples-data.s3.us-west-2.amazonaws.com/monitoring-insurance/training-data/insurance.csv",
                        "code": "https://github.com/oneconvergence/dkube-examples/tree/training/insurance",
-                       "model": "RandomForestRegressor"})
+                       "model": "Deep Neural Network"})
     
 print("Training Complete !")

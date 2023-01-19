7566cb3a090b9d00f8e8d9f4cf7129a162de2fac
Md Ahmed Khan <ahmed.khan@oneconvergence.com>
2023-01-19 18:32:40 +0530

    Add files via upload

---
+++ kafka-producer.py
@@ -0,0 +1,33 @@
+from kafka import KafkaProducer
+import json
+
+class MessageProducer:
+    broker = ""
+    topic = ""
+    producer = None
+
+    def __init__(self, broker, topic):
+        self.broker = broker
+        self.topic = topic
+        self.producer = KafkaProducer(
+                bootstrap_servers=self.broker,
+                value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+                acks='all',
+                retries = 3
+            )
+
+    def send_msg(self, msg):
+        print("sending message...")
+        try:
+            future = self.producer.send(self.topic,msg)
+            self.producer.flush()
+            future.get(timeout=60)
+            return {'status_code':200, 'error':None}
+        except Exception as ex:
+            return ex
+
+message_producer = MessageProducer("dkube-kafka-cp-kafka-headless.dkube-kafka:9092","loanreq")
+data = {"instances": [[1, 1, 0, 2, 0, 2, 0, 0, 36498, 36215, 6838, 0, 0, 33543, 54, 8313], [0, 2, 4, 2, 0, 13, 0, 0, 14370, 15181, 4601, 0, 0, 7657, 175, 7560]]}
+print(f"Sending data {data} to the kafka broker")
+resp = message_producer.send_msg(data)
+print(resp)

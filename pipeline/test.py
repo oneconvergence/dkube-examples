0b5501bafa4c0a3656d40527ebcd27d62a26d1b2
Larry Chisvin <42627414+larryc1200@users.noreply.github.com>
2023-04-13 16:23:33 -0600

    Update test.py

--- pipeline/test.py
+++ pipeline/test.py
@@ -8,6 +8,7 @@ import kfp.dsl as dsl
 import string
 
 dkube_training_op = components.load_component_from_url('https://raw.githubusercontent.com/oneconvergence/dkube/main/components/training/component.yaml')
+dkube_serving_op = components.load_component_from_url('https://raw.githubusercontent.com/oneconvergence/dkube/main/components/serving/component.yaml')
 
 training_program = "xray-larryc"
 training_script = "python chest-xray/training.py"
@@ -15,10 +16,11 @@ input_training_dataset = "xray-lc"
 model_name = "xray-lc"
 input_dataset_mount = ['/data']
 output_model_mount = "/model"
+serving_job_name = "ray-lc"
 
 @kfp.dsl.pipeline(
-    name="test",
-    description='xray-training-pl'
+    name="xray-pl",
+    description='Chest XRay Pipeline'
 )
 def xray_pipeline(token):    
     train       = dkube_training_op(container=json.dumps({"image": "ocdr/dkube-datascience-tf-cpu:v2.0.0-17"}),
@@ -30,3 +32,9 @@ def xray_pipeline(token):
                                     input_dataset_mounts=json.dumps(input_dataset_mount),
                                     output_mounts=json.dumps([str(output_model_mount)]),
                                     auth_token=token)
+    
+    serving     = dkube_serving_op(model=train.outputs['artifact'], device='cpu',
+                                    name=serving_job_name,
+                                    serving_image=json.dumps({"image": "ocdr/tensorflowserver:2.0.0"}),
+                                    auth_token=token, min_replicas = '1',
+                                    production="true").after(train)

c91beda370bcafb54e62733834e2221566ea1fe3
Larry Chisvin <42627414+larryc1200@users.noreply.github.com>
2023-04-14 11:24:24 -0600

    Update test.py

--- pipeline/test.py
+++ pipeline/test.py
@@ -13,7 +13,7 @@ token = "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc0YmNkZjBmZWJmNDRiOGRhZGQxZWIyOGM2MjhkYWY
 
 print("Setting up client")
 host_url = os.getenv("KF_PIPELINES_ENDPOINT")
-print(host_url)
+print("host_url =", host_url)
 
 client = kfp.Client(
     host=host_url,
